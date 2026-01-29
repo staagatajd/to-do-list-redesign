e0d6972dc760de47dddc3f9d5ee72154c231ff27
Jerome Daniel Sta. Agata <staagatadaniel@gmail.com>
2026-01-29 21:43:31 +0800

    Initial commit: Setup project structure

---
+++ README.md
@@ -0,0 +1,27 @@
+## Project Status: Work in Progress
+This project is currently under development as a part of my Web dev practice and also for my AppDev and EDP subjects.
+
+## Current To-Do List (Features):
+- [x] Created input field and Add button.
+- [x] Initial layout styling.
+- [x] Styled list elements (`<li>`).
+- [x] Implemented a scrollable container for long lists.
+
+## Features To Implement (1/28/26)
+- [ ] **Task Completion:** Add a checkbox that toggles a "Delete" option or strikes through the text.
+- [ ] **Delete Functionality:** Logic to remove specific `<li>` elements from the DOM/State.
+- [ ] **Persistence:** (Optional) Save tasks so they don't disappear on refresh.
+
+
+## Tech Stack
+Framework: React (Vite)
+Styling: CSS3 / Tailwind
+Logic: JavaScript (ES6+)
+Tools: Git, GitHub, npm
+
+## What I've learned Currently
+- **React Components & Functions:** How function works in react.
+- **State Flow:** State Management and Props.
+- **Advanced CSS:** Upskill my CSS skills (UI/UX).
+- **Independent Problem Solving:** Doing things with less AI help.
+- **Willingness To Learn** Learning things through asking AI and reasearching.
---
+++ src/MainToDoList.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+
+
+function MainToDoList() {
+
+    const [tasks, setTasks] = useState([]); //this creates an array for variable tasks, default value [].
+    const [inputValue, setInputValue] = useState(''); //this sets up the input value to ''. inputvalue as the input, setInputValue as the function/modifier.
+
+    //a function for the addBtn, it uses the state that useState created
+    const addTask = () => {
+        if(inputValue.trim()) //if the inputValue is not null or it has characters
+        {
+            setTasks([inputValue, ...tasks]); //function for tasks array or vector in c++ where it pushes elements in it. ...tasks is the elements, inputvalue is the pushback
+            setInputValue(''); //setting the input value on default affter adding.
+        }
+    };
+
+  return(
+
+    <div className='content'>
+        <div className= 'toDoListPage'> 
+            <h2>
+                My Tasks 📖
+            </h2>
+
+            <div className= 'inputSection'>
+                <input type = "text" id = "taskInput" value = {inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder= "Add new task..."></input>
+
+                <button onClick = {addTask} id='addBtn'>
+                    + Add
+                </button>
+            </div>
+            
+            <DisplayTask tasks = {tasks}/>
+
+            <div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+function DisplayTask({tasks})
+{
+    return(
+        <div className="displayScreen"> 
+            <ul id = "taskList">
+                {tasks.map((task, index) => (
+                    <li key = {index}>
+                        {task}
+                    </li>
+                ))} 
+            </ul>
+        </div>
+    )
+}
+
+export default App
